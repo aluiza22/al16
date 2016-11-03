85c6c345d5c3c0a1545d134cf3bc440090aff02d
root <root@ana-VirtualBox.(none)>
2016-11-03 20:12:45 -0200

    estou morrendo, alguem me ajude

--- app/code/community/Fishpig/Wordpress/Helper/Shortcode/Product.php
+++ app/code/community/Fishpig/Wordpress/Helper/Shortcode/Product.php
@@ -42,6 +42,18 @@ class Fishpig_Wordpress_Helper_Shortcode_Product extends Fishpig_Wordpress_Helpe
 					else if ($params->getIds()) {
 						$params->setIds(explode(',', $params->getIds()));
 					}
+					else if ($params->getSkus()) {
+						$ids = array();
+						$resource = Mage::getResourceModel('catalog/product');
+						
+						foreach(explode(',', $params->getSkus()) as $sku) {
+							if ($id = $resource->getIdBySku($sku)) {
+								$ids[] = $id;
+							}
+						}
+
+						$params->setIds($ids);
+					}
 					
 					if ($params->getIds()) {
 						$collection->addAttributeToFilter('entity_id', array('in' => $params->getIds()));
@@ -91,13 +103,14 @@ class Fishpig_Wordpress_Helper_Shortcode_Product extends Fishpig_Wordpress_Helpe
 					}
 					
 					$template = $params->getTemplate() ? $params->getTemplate() : 'wordpress/shortcode/product.phtml';
-						
-					$html = $this->_createBlock('catalog/product')
+
+					$html = $this->_createBlock('wordpress/shortcode_product')
 						->setTemplate($template)
 						->setItems($collection)
 						->setProducts($collection)
 						->setProduct($collection->getFirstItem())
 						->setProductId($collection->getFirstItem()->getId())
+						->setShortcodeParams($params)
 						->toHtml();
 
 					$content = str_replace($shortcode->getHtml(), $html, $content);
